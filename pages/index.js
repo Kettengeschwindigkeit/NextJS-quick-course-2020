398c67bd0e2eb680ff97594b0ca14809690388f8
Kettengeschwindigkeit <666damned999@mail.ru>
2023-05-07 12:40:26 +0300

    SEO + Meta

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Index() {
-    return <>
-        <h1>Hello NextJS!</h1>
-        <p><Link href={'/about'}><a>About</a></Link></p>
-        <p><Link href='/posts'><a>Posts</a></Link></p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-    </>
+    return (
+        <>
+            <Head>
+                <title>Next Title Index Hello</title>
+                <meta name='keywords' content='next, nextjs, javascript, react' />
+                <meta name='description' content='this is youtube tutorial for next' />
+                <meta charset='utf-8' />
+            </Head>
+            <h1>Hello NextJS!</h1>
+            <p><Link href={'/about'}><a>About</a></Link></p>
+            <p><Link href='/posts'><a>Posts</a></Link></p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+        </>
+    )
 }
