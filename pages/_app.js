5b95fef60c5adcb09542783f586d5911ab9340fa
Kettengeschwindigkeit <666damned999@mail.ru>
2023-05-08 12:36:39 +0300

    Loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import NextNProgress from 'nextjs-progressbar'
 import '../styles/main.scss'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress
+        color="yellow"
+        startPosition="0.3"
+        stopDelayMs="200"
+        height="3"
+      />
       <Component {...pageProps} />
     </>
   )
--- pages/about/index.js
+++ pages/about/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Router from 'next/router'
 import { MainLayout } from '../../components/MainLayout'
 
-export default function About() {
+export default function About({ title }) {
 
     const linkClickHandler = () => {
         Router.push('/')
@@ -10,9 +10,18 @@ export default function About() {
 
     return (
         <MainLayout title={'About Page'}>
-            <h1>About Page</h1>
+            <h1>{title}</h1>
             <button onClick={linkClickHandler}>Go back to home</button>
             <button onClick={() => Router.push('/posts')}>Go to posts</button>
         </MainLayout>
     )
+}
+
+About.getInitialProps = async () => {
+    const response = await fetch('http://localhost:4200/about')
+    const data = await response.json()
+
+    return {
+        title: data.title
+    }
 }
