bddc95fdfa1064fea8dacf4a2b8c308584cfc10b
Kettengeschwindigkeit <666damned999@mail.ru>
2023-05-08 11:48:05 +0300

    getInitialProps

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,10 @@
 import { useState, useEffect  } from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 import { MainLayout } from '../components/MainLayout'
 
-export default function Posts() {
-    const [posts, setPosts] = useState()
+export default function Posts({ posts: serverPosts }) {
+    const [posts, setPosts] = useState(serverPosts)
 
     useEffect(() => {
         async function load() {
@@ -12,8 +13,16 @@ export default function Posts() {
             setPosts(json)
         }
 
-        load()
+        if (!serverPosts) {
+            load()
+        }
     }, [])
+
+    if (!posts) {
+        return <MainLayout>
+          <p>Loading...</p>
+        </MainLayout>
+      }
 
     return (
         <MainLayout>
@@ -21,7 +30,30 @@ export default function Posts() {
                 <title>Posts Page | Next Course</title>
             </Head>
             <h1>Posts Page</h1>
-            <pre>{JSON.stringify(posts, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(posts, null, 2)}</pre> */}
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+                            <a>{post.title}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </MainLayout>
     )
+}
+
+Posts.getInitialProps = async ({ req }) => {
+
+    if (!req) {
+        return { posts: null }
+    }
+
+    const response = await fetch('http://localhost:4200/posts')
+    const posts = await response.json()
+
+    return {
+        posts
+    }
 }
